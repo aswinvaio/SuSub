51536d2678258cbd107d64ddd3c900c3d752abcc
aswinvaio <aswinprabhakaran@gmail.com>
2017-02-18 18:50:51 +0530

    QuickVarfy srch

---
+++ QuickVarfy/ExcelConfig.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuickVarfy
+{
+    /// <summary>
+    /// Interaction logic for ExcelConfig.xaml
+    /// </summary>
+    public partial class ExcelConfig : Window
+    {
+        public ExcelConfig()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
--- QuickVarfy/MainWindow.xaml.cs
+++ QuickVarfy/MainWindow.xaml.cs
@@ -15,6 +15,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
 using System.Threading;
+using OfficeOpenXml;
+using System.Data;
 
 namespace QuickVarfy
 {
@@ -23,6 +25,13 @@ namespace QuickVarfy
     /// </summary>
     public partial class MainWindow : Window
     {
+        string col_Name = "A";
+        string col_File = "B";
+        int row_Start = 2;
+        int row_End = -1;
+        bool col_configManually = false;
+        bool row_configManually = false;
+
         public MainWindow()
         {
             InitializeComponent(); 
@@ -65,11 +74,62 @@ namespace QuickVarfy
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
             this.Cursor = Cursors.Wait;
+            if (txtFile.Text != "" && txtDir.Text != "")
+                searchDirectory(txtFile.Text, txtDir.Text);
 
-
-
-            Thread.Sleep(2000);
+            //Thread.Sleep(2000);
             this.Cursor = Cursors.Arrow;
+        }
+
+        private bool searchDirectory(string fileName, string DirName)
+        {
+            //try
+            //{
+                using (ExcelPackage xlPackage = new ExcelPackage(new FileInfo(fileName)))
+                {
+                    ExcelWorksheet myWorksheet = xlPackage.Workbook.Worksheets.First(); //select sheet here
+                    int totalRows = myWorksheet.Dimension.End.Row;
+                    int totalColumns = myWorksheet.Dimension.End.Column;
+                    DataTable dt = new DataTable();
+                    dt.Columns.Add("Name");
+                    dt.Columns.Add("File");
+                    for (int i = row_Start; i <= totalRows; i++)
+                    {
+
+                        dt.Rows.Add(myWorksheet.Cells[i, 1].Text, myWorksheet.Cells[i, 2].Text);
+                    }
+                    List<string> listMIssedFiles = new List<string>();
+                    listMIssedFiles.Add("Found : ");
+                    int found = 0;
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        string filepath = DirName + "\\" + dr[1].ToString();
+                        if (!File.Exists(filepath))
+                        {
+                            listMIssedFiles.Add(dr[0].ToString() + " : " + dr[1].ToString() + " is missing ");
+                        }
+                        else
+                        {
+                            found++;
+                        }
+                    }
+                    listMIssedFiles[0] += found;
+                    lbResult.ItemsSource = listMIssedFiles;
+                }
+            //}
+            //catch (Exception err)
+            //{
+            //    MessageBox.Show(err.Message, "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            //    return false;
+            //}
+            return true;
+        }
+
+        private void btnExcelConfig_Click(object sender, RoutedEventArgs e)
+        {
+            ExcelConfig ec = new ExcelConfig();
+            ec.Owner = this;
+            ec.ShowDialog();
         }
     }
 }
